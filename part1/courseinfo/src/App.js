3db33ca84e4c27d911d86a18ac3e72ede4f44c0f
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-01-25 12:54:39 +0100

    part 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -26,7 +26,8 @@ const Content = (props) => {
 
 const Total = (props) => {
   const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
-    
+  
+
   return (
     <p>Number of exercises {total}</p>
   )
